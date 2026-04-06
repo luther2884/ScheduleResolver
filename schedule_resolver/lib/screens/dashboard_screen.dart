3b55614a8efe84b4f4dc3de50c791b8960fab3c8
lutherrav <ravelolutherl@gmail.com>
2026-04-06 13:29:12 +0800

    detect conflicts, rank priorities, and generate a revised schedule.

---
+++ schedule_resolver/lib/screens/dashboard_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/schedule_provider.dart';
+import '../services/ai_schedule_service.dart';
+import '../models/task_model.dart';
+import 'task_input_screen.dart';
+import 'recommendation_screen.dart';
+
+class DashboardScreen extends StatelessWidget{
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final scheduleProvider = Provider.of<ScheduleProvider>(context);
+    final aiService = Provider.of<AiScheduleService>(context);
+
+    final sortedTasks = List<TaskModel>.from(scheduleProvider.task);
+    sortedTasks.sort((a,b) => a.startTime.hour.compareTo(b.startTime.hour));
+
+  return Scaffold(
+    appBar: AppBar(title: const Text('Schedule Resolver'), centerTitle: true,),
+  );
+
+  }
+}
---
+++ schedule_resolver/lib/services/ai_schedule_service.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import '../models/task_model.dart';
+import '../models/schedule_analysis.dart';
+
+
+class AiScheduleService extends ChangeNotifier {
+
+  ScheduleAnalysis? _currentAnalysis;
+  bool _isLoading = false;
+  String? _errorMessage;
+
+  final String _apikey = '';
+
+  ScheduleAnalysis? get currentAnalysis => _currentAnalysis;
+  bool get isLoading => _isLoading;
+  String? get errorMessage => _errorMessage;
+
+  Future<void> analyzeSchedule(List<TaskModel> task) async {
+    if (_apikey.isEmpty || task.isEmpty) return;
+    _isLoading = true;
+    _errorMessage = null;
+    notifyListeners();
+
+    try {
+      final model = GenerativeModel(model: 'gemini-2.5-flash', apiKey: _apikey);
+      final taskJson = jsonEncode(task.map((t) => t.toJson()).toList());
+
+      final prompt = '''
+      
+      You are an expert student scheduling assistant. The user has provided the following task for their day in JSON format:
+      
+      \$askjson
+      
+      Please provide exactly 4 section of markdown text:
+      1. ### Detected Conflicts
+      List any scheduling conflicts or state that there are none.
+      2. ### Ranked Tasks
+      Ranks which tasks needed attention first.
+      3. ### Recommended Schedule
+      Provide a revised daily timeline view adjusting the task times
+      4. ### Explanation
+      Explain why this recommendation was made.
+      
+      ''';
+
+      final content = [Content.text(prompt)];
+      final response = await model.generateContent(content);
+
+      _currentAnalysis = _parseResponse(response.text ?? '');
+    } catch (e) {
+
+      _errorMessage = 'Failed: \$e';
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+
+  }
+
+  ScheduleAnalysis _parseResponse(String fullText) {
+    String conflicts = '', rankedTask = '',
+    recommendedSchedule = '',
+    explanation = '';
+
+    final sections = fullText.split('###');
+    for (var section in sections) {
+      if (section.startsWith('Detected Conflicts')) conflicts = section.replaceFirst('Detected Conflicts', '').trim();
+      else if (section.startsWith('Ranked Tasks')) rankedTask = section.replaceFirst('Ranked Tasks', '').trim();
+      else if (section.startsWith('Recommended Schedule')) recommendedSchedule = section.replaceFirst('Recommended Schedule', '').trim();
+      else if (section.startsWith('Explanation')) explanation = section.replaceFirst('Explanation', '').trim();
+
+    }
+
+    return ScheduleAnalysis(
+        conflicts: conflicts,
+        rankedTasks: rankedTask,
+        recommendedSchedule: recommendedSchedule,
+        explanation: explanation
+    );
+  }
+
+
+}
